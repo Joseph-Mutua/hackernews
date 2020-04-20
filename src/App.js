ff94b485ac7c37b475812051ed89e16f267e6607
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-20 18:06:50 +0300

    Add internal component state

--- src/App.js
+++ src/App.js
@@ -19,12 +19,19 @@ const list = [
   objectID: 1,
   },
   ];
-   
+ 
   class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+      };
+    }
     render() {
     return (
     <div className="App">
-{list.map(item =>
+{this.state.list.map(item =>
 <div key={item.objectID}>
 <span>
 <a href={item.url}>{item.title}</a>
@@ -38,4 +45,6 @@ const list = [
     );
     }
 }
+
+
 export default App;
