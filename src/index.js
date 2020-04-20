d5c0482bffd264333c1f5e47a80826b81f607c44
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-20 22:47:51 +0300

    Add event handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-ReactDOM.render(
-<App />,
-document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 if (module.hot) {
-module.hot.accept();
+  module.hot.accept();
 }
